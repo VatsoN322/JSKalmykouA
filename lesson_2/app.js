cc3c62f54e8552a87d95b3bad7d23d06d9d62790
DESKTOP-97R2J66\Artyr <artyr4ikan777@gmail.com>
2022-04-29 00:33:04 +0300

    comlpete task 2

--- lesson_2/app.js
+++ lesson_2/app.js
@@ -1,4 +1,5 @@
 'use strict'
+/*
 //Задание 1
 const fName = prompt("Write your first name:");
 const sName = prompt("Write your surname:");
@@ -19,3 +20,26 @@ else
 {
     console.log(`Здравствуйте, Noname`);
 }
+*/
+//Задание 2
+const num = +prompt("Write a number:");
+let message = "Введенное вами число ";
+if(num > 100)
+    message += "больше ста, ";
+else if(num == 100)
+    message += "равно ста, ";
+else
+    message += "меньше ста, ";
+if(num%2 == 0)
+    message += "четное, ";
+else
+    message += "нечетное, ";
+if(num < 0)
+    message += "отрицательное, ";
+else
+    message += "положительное, ";
+if(num - Math.trunc(num) != 0)
+    message += "дробное";
+else
+    message += "недробное";
+console.log(message);
